0342b118986d70b4aa4524322cd6f9319b120b41
ftomara <ftomara647@gmail.com>
2025-12-05 14:45:03 +0200

    viginere cipher done

--- Scenes/CipherScene.gd
+++ Scenes/CipherScene.gd
@@ -13,13 +13,11 @@ extends Control
 @export var width = 5
 @export var height = 5
 
-@export var Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 var defaultMenuName = ""
-var PlainText = ""
-var KEY = ""
+
 
 func _ready():
+	#get_tree().change_scene_to_file("res://Scenes/ViginereCipher.tscn")
 	defaultMenuName = "Choose Cipher"
 	
 	
@@ -29,8 +27,8 @@ func _on_choose_cipher_pressed():
 func _set_cipher_name(CipherName : String):
 	choose_cipher.text = CipherName
 	
-func _on_hill_cipher_pressed():
-	_set_cipher_name("Hill Cipher")
+func _on_ceaser_cipher_pressed():
+	_set_cipher_name("Ceaser")
 	cipher_menu.visible = false
 	_get_input()
 	
@@ -41,15 +39,14 @@ func _on_reset_pressed():
 	choose_cipher.text = defaultMenuName
 	
 func _Create_PlayFair_Matrix():
-	
 	for i in 25:
 		var new_cell = cell.instantiate()
-		if(KEY != ""):
+		if(Global.KEY != ""):
 			if(i<5):
-				new_cell.get_child(0).get_child(0).text = KEY[i]
+				new_cell.get_child(0).get_child(0).text = Global.KEY[i]
 				new_cell.get_child(0).get_child(0).set("theme_override_colors/font_color", Color.YELLOW)
 			else:
-				new_cell.get_child(0).get_child(0).text = Alphabet[i]
+				new_cell.get_child(0).get_child(0).text = Global.Alphabet[i]
 			grid_container.add_child(new_cell)
 			_create_Array2D()
 	#_print()
@@ -66,12 +63,27 @@ func _print():
 			print(Array2D[i][j].get_child(0).get_child(0).text)
 
 func _get_input():
-	PlainText = text_entry.text
-	KEY = key_entry.text
-	print("plain text : " , PlainText)
+	Global.PlainText = text_entry.text
+	Global.KEY = key_entry.text
+	print("plain text : " , Global.PlainText)
 
 func _on_play_fair_pressed():
 	cipher_menu.visible = !cipher_menu.visible
 	_set_cipher_name("Play Fair")
 	_get_input()
 	_Create_PlayFair_Matrix()
+
+
+func _on_viginere_cipher_pressed() -> void:
+	_set_cipher_name("Viginere")
+	cipher_menu.visible = false
+	_get_input()
+	_create_viginere()
+
+func _create_viginere() ->void:
+	get_tree().change_scene_to_file("res://Scenes/ViginereCipher.tscn")
+
+func _on_des_cipher_pressed() -> void:
+	_set_cipher_name("DES")
+	cipher_menu.visible = false
+	_get_input()
---
+++ Scenes/viginere_cipher.gd
@@ -0,0 +1,108 @@
+extends Control
+
+var previous_size
+@export var Array2D = []
+@onready var viginere_cell = preload("res://Scenes/viginere_cell.tscn")
+@onready var grid = $ViginereGrid
+@onready var halphabet = $Halphabet
+@onready var valphabet = $Valphabet
+@onready var key = $Panel/Key2
+@onready var plain_text = $Panel/PlainText2
+@onready var cipher_text = $Panel/CipherText2
+const highlight_style = preload("res://Assets/new_style_box_flat.tres")
+const found_style = preload("res://Assets/green_style_box_flat.tres")
+
+func _ready() -> void:
+	previous_size = DisplayServer.window_get_size()
+	
+	DisplayServer.window_set_size(Vector2i(2500, 1800))
+	
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+	DisplayServer.window_set_position((screen_size - window_size) / 2)
+	_create_viginere()
+
+func _create_viginere() ->void:
+	_set_labels()
+	for i in 26 :
+		var hlabel = _create_alpha_label(Global.Alphabet[i],false)
+		halphabet.add_child(hlabel)
+		
+		var vlabel = _create_alpha_label(Global.Alphabet[i],true)
+		valphabet.add_child(vlabel)
+		
+		var k = i
+		for j in 26 :
+			var new_cell = viginere_cell.instantiate()
+			new_cell.get_child(0).get_child(0).text = Global.Alphabet[k%26]
+			k = k+1
+			grid.add_child(new_cell)
+	_create_Array2D()
+	_encrypt()
+
+func _create_Array2D():
+	for i in 26 :
+		Array2D.append([])
+		for j in 26:
+			Array2D[i].append(grid.get_child(i * 26 + j))
+
+func _create_alpha_label(letter : String , isV :bool) -> Label :
+	var label = Label.new()
+	label.text = letter
+	label.add_theme_font_size_override("font_size", 45)
+	label.add_theme_font_override("font", load("res://Assets/Pixel Game.otf"))
+	return label
+	
+
+func _set_labels()->void:
+	if Global.KEY == "" or Global.PlainText == "" :
+		return
+	plain_text.text = Global.PlainText
+	key.text = Global.KEY
+	while key.text.length() < Global.PlainText.length() :
+		key.text += Global.KEY
+	key.text =  key.text.substr(0,Global.PlainText.length())
+
+func _encrypt()->void:
+	var col_index 
+	var row_index
+	for i in key.text.length():
+		await get_tree().create_timer(0.5).timeout
+		col_index = Global.AlphabetMap[key.text[i]]-1
+		row_index = Global.AlphabetMap[plain_text.text[i]]-1
+		halphabet.get_child(col_index).set("theme_override_colors/font_color", Color.YELLOW)
+		valphabet.get_child(row_index).set("theme_override_colors/font_color", Color.YELLOW)
+		#animate cell coloring
+		for k in 26 :
+			Array2D[row_index][k].get_child(0).get_child(0).set("theme_override_styles/normal", highlight_style)
+			Array2D[k][col_index].get_child(0).get_child(0).set("theme_override_styles/normal", highlight_style)
+			await get_tree().create_timer(0.1).timeout
+		
+		Array2D[row_index][col_index].get_child(0).get_child(0).set("theme_override_styles/normal", found_style)
+		await get_tree().create_timer(0.2).timeout
+		
+		# reset cells
+		for k in 26 :
+				Array2D[row_index][k].get_child(0).get_child(0).set("theme_override_styles/normal",StyleBoxEmpty.new())
+				Array2D[k][col_index].get_child(0).get_child(0).set("theme_override_styles/normal",StyleBoxEmpty.new())
+							
+		await get_tree().create_timer(0.5).timeout
+		cipher_text.text +=Array2D[row_index][col_index].get_child(0).get_child(0).text
+		halphabet.get_child(col_index).set("theme_override_colors/font_color", Color.WHITE)
+		valphabet.get_child(row_index).set("theme_override_colors/font_color", Color.WHITE)
+		
+	#print(cipher_text.text)
+	#for i in 26 :
+		#for j in 26:
+			#print(i)
+			#print(j)
+			#print(Array2D[i][j].get_child(0).get_child(0).text)
+			#print("===============================")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/CipherScene.tscn")
+	DisplayServer.window_set_size(previous_size)
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+	DisplayServer.window_set_position((screen_size - window_size) / 2)
