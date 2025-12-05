39e36401e98a1ae532d795a98d9fad1c6bbb04bf
Menna Jaheen <menna.jaheen16@gmail.com>
2025-12-05 20:21:28 +0200

    Removed Key duplicates and added the remained letters|last letter isn't handled

--- Scipts/CipherScene.gd
+++ Scipts/CipherScene.gd
@@ -14,12 +14,15 @@ extends Control
 @export var height = 5
 
 var defaultMenuName = ""
-
+var end = 0
 
 func _ready():
 	#get_tree().change_scene_to_file("res://Scenes/ViginereCipher.tscn")
 	defaultMenuName = "Choose Cipher"
+	for i in range(255):
+		Global.arr.append(0)
 	
+
 	
 func _on_choose_cipher_pressed():
 	cipher_menu.visible = !cipher_menu.visible
@@ -39,17 +42,37 @@ func _on_reset_pressed():
 	choose_cipher.text = defaultMenuName
 	
 func _Create_PlayFair_Matrix():
+	_remove_KEY_duplicates()
 	for i in 25:
 		var new_cell = cell.instantiate()
 		if(Global.KEY != ""):
-			if(i<5):
-				new_cell.get_child(0).get_child(0).text = Global.KEY[i]
+			if(i<Global.keyArr.size()):
+				new_cell.get_child(0).get_child(0).text = Global.keyArr[i]
 				new_cell.get_child(0).get_child(0).set("theme_override_colors/font_color", Color.YELLOW)
 			else:
-				new_cell.get_child(0).get_child(0).text = Global.Alphabet[i]
+				#print("Array size : ",Global.keyArr.size(),"i = ",i,"Alphabet : ",Global.Alphabet[i],"  uni code of alphabet : ", Global.Alphabet.unicode_at(i))
+				if Global.arr[ Global.Alphabet.unicode_at(i-Global.keyArr.size()) ] == 0:
+					new_cell.get_child(0).get_child(0).text = Global.Alphabet[i-Global.keyArr.size()]
+				else:
+					continue
 			grid_container.add_child(new_cell)
-			_create_Array2D()
-	#_print()
+		end = i
+		print("End = " , end)
+			
+	for i in range(25 - Global.keyArr.size(), 25):
+		var new_cell = cell.instantiate()
+		if i == 23:
+			new_cell.get_child(0).get_child(0).text = "99"
+			break
+		if Global.arr[ Global.Alphabet.unicode_at(i) ] == 0:
+			new_cell.get_child(0).get_child(0).text = Global.Alphabet[i]
+		else:
+			continue
+		grid_container.add_child(new_cell)
+	
+	var lastCell = cell.instantiate()
+	#lastCell.get_child(0).get_child(0).text = Global.Alphabet[24] + "/" + Global.Alphabet[25]
+	_create_Array2D()
 	
 func _create_Array2D():
 	for i in 5 :
@@ -65,6 +88,7 @@ func _print():
 func _get_input():
 	Global.PlainText = text_entry.text
 	Global.KEY = key_entry.text
+	_remove_KEY_duplicates()
 	print("plain text : " , Global.PlainText)
 
 func _on_play_fair_pressed():
@@ -73,6 +97,16 @@ func _on_play_fair_pressed():
 	_get_input()
 	_Create_PlayFair_Matrix()
 
+func _remove_KEY_duplicates():
+	for i in Global.KEY.length():
+		var freq = Global.arr[Global.KEY.unicode_at(i)]
+		if(freq == 0):		
+			Global.arr[Global.KEY.unicode_at(i)] +=1
+			Global.keyArr.append(Global.KEY[i])
+
+	for i in Global.keyArr.size():
+		print(Global.keyArr[i])	
+		
 
 func _on_viginere_cipher_pressed() -> void:
 	_set_cipher_name("Viginere")
---
+++ Scipts/global.gd
@@ -0,0 +1,61 @@
+extends Node
+var PlainText = ""
+var KEY = ""
+var keyArr = []
+var arr = []
+@export var Alphabet = "abcdefghijklmnopqrstuvwxyz"
+@export var AlphDict :={	
+	"a": 0,
+	"b": 0,
+	"c": 0,
+	"d": 0,
+	"e": 0,
+	"f": 0,
+	"g": 0,
+	"h": 0,
+	"i": 0,
+	"j": 0,
+	"k": 0,
+	"l": 0,
+	"m": 0,
+	"n": 0,
+	"o": 0,
+	"p": 0,
+	"q": 0,
+	"r": 0,
+	"s": 0,
+	"t": 0,
+	"u": 0,
+	"v": 0,
+	"w": 0,
+	"x": 0,
+	"y": 0,
+	"z": 0}
+@export var AlphabetMap := {
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+	"f": 6,
+	"g": 7,
+	"h": 8,
+	"i": 9,
+	"j": 10,
+	"k": 11,
+	"l": 12,
+	"m": 13,
+	"n": 14,
+	"o": 15,
+	"p": 16,
+	"q": 17,
+	"r": 18,
+	"s": 19,
+	"t": 20,
+	"u": 21,
+	"v": 22,
+	"w": 23,
+	"x": 24,
+	"y": 25,
+	"z": 26
+}
